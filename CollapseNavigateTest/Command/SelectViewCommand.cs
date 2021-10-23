456704f4f7177d87f161837503ddad0daf03d160
RKC02 <dinhlockt02@gmail.com>
2021-10-23 11:51:07 +0700

    Add project files.

---
+++ CollapseNavigateTest/Command/SelectViewCommand.cs
@@ -0,0 +1,31 @@
+﻿using CollapseNavigateTest.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CollapseNavigateTest.Command
+{
+    class SelectViewCommand : ICommand
+    {
+        private MainWindowViewModel mainWindowViewModel;
+        public SelectViewCommand(MainWindowViewModel mainWindowViewModel)
+        {
+            this.mainWindowViewModel = mainWindowViewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Console.WriteLine(parameter.ToString());
+        }
+    }
+}
